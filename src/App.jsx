b4c113ab432a8dd5e40b393b6adbbf3090cb8b5b
Giuseppe Tomasini <giusetomasini@gmail.com>
2022-07-07 14:44:48 -0500

    setup Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import BookList from './components/book-list/BookList';
 import './App.scss';
+import { Routes, Route } from 'react-router-dom';
+import BookList from './components/book-list/BookList';
+import Navbar from './components/navbar/Navbar';
+import Categories from './pages/Categories';
 
 export default function App() {
   return (
     <>
-      <div>App</div>
-      <BookList />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<BookList />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
     </>
   );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav>
+      <p className="logo-text">
+        Bookstore CMS
+      </p>
+      <ul className="links">
+        <li><Link to="/" className="link">BOOKS</Link></li>
+        <li><Link to="/categories" className="link">CATEGORIES</Link></li>
+      </ul>
+    </nav>
   );
 }
