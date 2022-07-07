6204e4db0455129be4984b27bcc095cc42236e98
Giuseppe Tomasini <giusetomasini@gmail.com>
2022-07-07 13:05:02 -0500

    setup files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import BookList from './components/book-list/BookList';
 
 export default function App() {
   return (
-    <div>App</div>
+    <>
+      <div>App</div>
+      <BookList />
+    </>
   );
 }
---
+++ src/components/book-list/BookList.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './BookList.scss';
+
+export default function BookList() {
+  return (
+    <div>BookList</div>
+  );
+}
