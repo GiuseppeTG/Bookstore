4a00081ae3e07c2e05fafaba9098e6c61c6fd650
Giuseppe Tomasini <giusetomasini@gmail.com>
2022-07-07 14:04:50 -0500

    Components file setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import BookList from './components/book-list/BookList';
+import './App.scss';
 
 export default function App() {
   return (
--- src/components/book/Book.jsx
+++ src/components/book/Book.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './Books.scss';
 
 export default function book() {
   return (
